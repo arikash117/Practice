899aae1436a7bd7f513a2f75971956565b19a32d
arikash117 <ari.kash117@gmail.com>
2024-06-27 20:17:01 +0300

    changed table columns

--- Backend/DataBase.sql
+++ Backend/DataBase.sql
@@ -1,9 +1,9 @@
 CREATE TABLE vacancies (
     vacancy_ID INT,
     vacancy TEXT,
-    place TEXT,
-    work_time TEXT,
+    area TEXT,
+    schedule TEXT,
+    employment TEXT,
     experience TEXT,
-    industry TEXT,
-    salary TEXT,
+    employer TEXT,
 );
