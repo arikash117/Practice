fc4c7062e3fe43b544489fd2610f2e79ff7fa65e
arikash117 <ari.kash117@gmail.com>
2024-06-27 18:30:22 +0300

    added area_list

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,19 @@
 import requests
 
 
-# список вакансии
 response = requests.get('https://api.hh.ru/vacancies')
 
+# vacancy list
 vacancy_list = []
 vacancies = response.json()['items']
 for vacancy in vacancies:
     name = vacancy['name']
     vacancy_list.append(name)
-print(vacancy_list)
+
+
+# area list
+area_list = []
+areas = response.json()['items']
+for area in areas:
+    name = area['area']['name']
+    area_list.append(name)
