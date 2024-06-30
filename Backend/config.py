dc272511b1ee0b6dffe58e77aec204233575b37d
arikash117 <ari.kash117@gmail.com>
2024-06-30 19:42:01 +0300

    new database

--- Backend/config.py
+++ Backend/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = 'mysql+pymysql://root:root@127.0.0.1:3306/Vacancies_data'
+    db_url: str = 'mysql+pymysql://root:root@127.0.0.1:3306/vacancies_data'
 
 
 settings = Settings()
