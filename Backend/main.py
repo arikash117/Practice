195d20b0341ba1f399bde243b895758eea55cfaa
arikash117 <ari.kash117@gmail.com>
2024-06-30 19:15:49 +0300

    changed code

--- Backend/main.py
+++ Backend/main.py
@@ -1,53 +1,31 @@
 import requests
+from config import settings
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
 response = requests.get('https://api.hh.ru/vacancies')
+engine = create_engine(settings.db_url)
+Base = declarative_base()
 
-# vacancy list
-vacancy_list = []
+class infoVacancyTable(Base):
+    __tablename__ = 'Vacancies_data'
+    id = Column(Integer, primary_key=True)
+    area = Column(String(100), nullable=False)
+    schedule = Column(String(100), nullable=False)
+    employer = Column(String(100), nullable=False)
+    experience = Column(String(100), nullable=False)
+
+Base.metadata.create_all(engine)
+
 vacancies = response.json()['items']
 for vacancy in vacancies:
+    id = vacancy['id']
     name = vacancy['name']
-    vacancy_list.append(name)
-
-# vacancy id list
-vacancy_id_list = []
-for vacancy_id in vacancies:
-    name = vacancy_id['id']
-    vacancy_id_list.append(name)
-
-
-# area list
-area_list = []
-for area in vacancies:
-    name = area['area']['name']
-    area_list.append(name)
-
-
-# schedule list
-schedule_list = []
-for schedule in vacancies:
-    name = schedule['schedule']['name']
-    schedule_list.append(name)
-
-
-# employment list
-employment_list = []
-for employment in vacancies:
-    name = employment['employment']['name']
-    employment_list.append(name)
-
-
-# experience list
-experience_list = []
-for experience in vacancies:
-    name = experience['experience']['name']
-    experience_list.append(name)
-
-
-# employer list
-employer_list = []
-for employer in vacancies:
-    name = employer['employer']['name']
-    employer_list.append(name)
+    area = vacancy['area']['name']
+    schedule = vacancy['schedule']['name']
+    employer = vacancy['employer']['name']
+    experience = vacancy['experience']['name']
 
