e9b9e5b6464ea8c36c6a77c309640040e405087a
arikash117 <ari.kash117@gmail.com>
2024-07-01 23:51:45 +0300

    added /search first state

--- Frontend/routers/__init__.py
+++ Frontend/routers/__init__.py
@@ -1,9 +1,11 @@
 from aiogram import Router
 from .commands import router as commands_router
+from .search_cmd import router as search_router
 
 router = Router(name=__name__)
-router.include_router(
+router.include_routers(
     commands_router,
+    search_router,
 )
 
 __all__ = (
---
+++ Frontend/routers/search_cmd.py
@@ -0,0 +1,18 @@
+from aiogram import Router, types
+from aiogram.enums import ChatAction
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+
+class Form(StatesGroup):
+    first = State()
+
+router = Router(name=__name__)
+
+
+@router.message(Command('search'))
+async def hadler_search(message: types.Message, state: FSMContext)
+    await state.set_state(Form.first)
+    await message.answer("What vacancy do you want  to search?")
+    
