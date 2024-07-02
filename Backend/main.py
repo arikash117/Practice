3a11c554049a9eec32ccd9716041735e29cafa58
arikash117 <ari.kash117@gmail.com>
2024-07-02 19:47:07 +0300

    added searching_vac func

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,12 @@
 import requests
+import mysql.connector
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-response = requests.get('https://api.hh.ru/vacancies')
+
 engine = create_engine('mysql+pymysql://root:root@127.0.0.1:3306/vacancies_data')
 Base = declarative_base()
 
@@ -20,11 +21,37 @@ class Table(Base):
 
 Base.metadata.create_all(engine)
 
-vacancies = response.json()['items']
-for vacancy in vacancies:
-    id = vacancy['id']
-    name = vacancy['name']
-    area = vacancy['area']['name']
-    schedule = vacancy['schedule']['name']
-    employer = vacancy['employer']['name']
-    experience = vacancy['experience']['name']
+def searching_vac(vac_name, query):
+    response = requests.get('https://api.hh.ru/vacancies')
+    vacancy_flt = vac_name.lower()
+    filtered_vacancies = []
+    vacancies = response.json()['items']
+    for vacancy in vacancies:
+        name = vacancy['name']
+        area = vacancy['area']['name']
+        schedule = vacancy['schedule']['name']
+        employer = vacancy['employer']['name']
+        experience = vacancy['experience']['name']
+
+    if vacancy_flt in name.lower():
+        new_vacancy_data = Table(
+            name = name,
+            area = area,
+            schedule = schedule,
+            employer = employer,
+            experience = experience,
+        )
+        Base.session.add(new_vacancy_data)
+        Base.session.commit()
+
+        filtered_vacancies.append({
+            'name': name,
+            'area': area,
+            'schedule': schedule,
+            'employer': employer,
+            'experience': experience
+        })
+        if query == 'all':
+            return filtered_vacancies
+        else:
+            return filtered_vacancies(name)
