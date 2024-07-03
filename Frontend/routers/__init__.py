0688528ff167303290d5074060d4d5bf8cd69f02
arikash117 <ari.kash117@gmail.com>
2024-07-03 14:12:33 +0300

    added router for info_cmd

--- Frontend/routers/__init__.py
+++ Frontend/routers/__init__.py
@@ -1,11 +1,13 @@
 from aiogram import Router
 from .commands import router as commands_router
 from .search_cmd import router as search_router
+from .info_cmd import router as info_router
 
 router = Router(name=__name__)
 router.include_routers(
     commands_router,
     search_router,
+    info_router,
 )
 
 __all__ = (
